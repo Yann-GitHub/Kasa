f3290db81a89dd230e7ed1b0b83aaa53219b8188
Yann <ybarlet@gmail.com>
2023-02-21 21:09:54 +0100

    Code refactoring

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,37 +1,9 @@
-// import { useState, useEffect } from 'react'
 import Card from '../../components/Card'
 import Banner from '../../components/Banner'
-// import Loader from '../../components/Loader'
-// import { fetchData } from '../../services/FetchData'
 import AccommodationsData from '../../data/logements.json'
 import '../../style/pages/Home.css'
 
 function Home() {
-  // const [isDataLoading, setDataLoading] = useState(false)
-  // const [error, setError] = useState(false)
-  // const [accommodationsData, setAccommodationsData] = useState([])
-
-  // useEffect(() => {
-  //   async function getAccommodationsData() {
-  //     setDataLoading(true)
-  //     try {
-  //       const response = await fetch('../../data/logements.json')
-  //       const { accommodationsData } = await response.json()
-  //       setAccommodationsData(accommodationsData)
-  //     } catch (error) {
-  //       console.error(error)
-  //       setError(true)
-  //     } finally {
-  //       setDataLoading(false)
-  //     }
-  //   }
-  //   getAccommodationsData()
-  // }, [])
-
-  // if (error) {
-  //   return <span>Oups il y a eu un problème</span>
-  // }
-
   return (
     <div className="main-wrapper-home">
       <Banner page="home" />
